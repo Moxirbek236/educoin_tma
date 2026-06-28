d98e07359b8ea2a07d194abe62b2f2e66c5c51d5
Moxirbek Solijonov <moxirbekmoxirbek29@gmail.com>
2026-06-28 09:51:26 +0500

    fix

--- src/theme.ts
+++ src/theme.ts
@@ -18,7 +18,7 @@ export const theme = createTheme({
     },
   },
   shape: {
-    borderRadius: 10.4, 
+    borderRadius: 10.4, // roughly 0.65rem
   },
   typography: {
     fontFamily: '"Inter", "Roboto", "Helvetica", "Arial", sans-serif',
